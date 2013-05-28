e0453a179a833048cdf2a4689b3fd65180213143
c <cd c>
2013-05-27 20:09:53 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+UPCTwitter::Application.config.secret_token = '65b7f575b2f1168e1eea2c15434f597589b7eeac872c9eb9c434f6c23848614d00f000028ee250bc1f1d23e61e09fa14c425bfa81645942f9f81505b07a0d438'
