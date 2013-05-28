94670db472c84fffc8851da03fac055e239c53fc
mfrancia <manuelfrancia678@gmail.com>
2013-05-27 20:14:37 -0500

    add twitter key

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,2 +1,8 @@
 class Tweet < ActiveRecord::Base
+	Twitter.configure do |config|
+	  config.consumer_key = 'GIQzUwZdDB5ACfCBenJMQ'
+	  config.consumer_secret = 'LBNzZuWH9yDCp2Z2ieT0BbWmcSXS68bYuBZpHsmW7QM'
+	  config.oauth_token = '1463450034-1NPYyV77gzhTDxiPzwwSVZl4p2ZCrf68giVK7UL'
+	  config.oauth_token_secret = 'krAWo4r2u1re49MvL0DyWEeiYaUCAV4uCXFMVnzvY'
+	end
 end
